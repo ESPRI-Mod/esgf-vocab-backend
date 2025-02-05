0044f689a76bac779771f66745e417cc3e78eda1
Sébastien Gardoll <sebastien@gardoll.fr>
2025-02-05 17:08:55 +0100

    test: automate tests

--- tests/test_universe_api.py
+++ tests/test_universe_api.py
@@ -1,17 +1,24 @@
 import json
 
-import requests as r
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from esgvoc_backend.universe import router
 
 _BASE_URL = 'http://localhost:9999/universe'
 
+app = FastAPI()
+app.include_router(router)
+client = TestClient(app, base_url=_BASE_URL, backend='asyncio')
+
 
 def _test_get(url: str, min_items: int, select: bool):
-    result = r.get(url=url)
+    result = client.get(url=url)
     result.raise_for_status()
     assert len(result.json()) > min_items
     if select:
         params = {'selected_term_fields': ['drs_name']}
-        result = r.get(url=url, params=params)
+        result = client.get(url=url, params=params)
         result = result.json()
         assert len(result) > min_items
         assert len(result[min_items]) == 3
@@ -19,10 +26,10 @@ def _test_get(url: str, min_items: int, select: bool):
 
 def _test_find(url: str, params: dict[str: str], select: bool):
     settings = {'case_sensitive': False, 'selected_term_fields': ['drs_name']}
-    result = r.get(url=url, params=params)
+    result = client.post(url=url, params=params)
     result.raise_for_status()
     assert len(result.json()) == 0
-    result = r.get(url=url, params=params, data=json.dumps(settings))
+    result = client.post(url=url, params=params, data=json.dumps(settings))
     result.raise_for_status()
     result = result.json()
     assert len(result) == 1
@@ -31,38 +38,38 @@ def _test_find(url: str, params: dict[str: str], select: bool):
 
 
 def test_terms_in_universe_get() -> None:
-    url = _BASE_URL + '/terms'
+    url = '/terms'
     min_items = 2000
     _test_get(url, min_items, True)
 
 
 def test_terms_in_universe_find() -> None:
-    url = _BASE_URL + '/terms/find'
+    url = '/terms/find'
     params = {'term_id': 'IpsL'}
     _test_find(url, params, True)
 
 
 def test_data_descriptors_get():
-    url = _BASE_URL + '/data_descriptors'
+    url = '/data_descriptors'
     min_items = 10
     _test_get(url, min_items, False)
 
 
 def test_data_descriptors_find() -> None:
-    url = _BASE_URL + '/data_descriptors/find'
+    url = '/data_descriptors/find'
     params = {'data_descriptor_id': 'InstitutioN'}
     _test_find(url, params, False)
 
 
 def test_terms_in_data_descriptor_get() -> None:
     data_descriptor_id = 'institution'
-    url = _BASE_URL + f'/data_descriptors/{data_descriptor_id}/terms'
+    url = f'/data_descriptors/{data_descriptor_id}/terms'
     min_items = 10
     _test_get(url, min_items, True)
 
 
 def test_terms_in_data_descriptor_find() -> None:
     data_descriptor_id = 'institution'
-    url = _BASE_URL + f'/data_descriptors/{data_descriptor_id}/terms/find'
+    url = f'/data_descriptors/{data_descriptor_id}/terms/find'
     params = {'term_id': 'IpsL'}
     _test_find(url, params, True)
