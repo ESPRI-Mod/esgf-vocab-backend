0a6b7e66e9561434ca4016031a9961370bcbacd8
Sébastien Gardoll <sebastien@gardoll.fr>
2025-02-12 19:14:29 +0100

    feat: add processing time in the response header

--- src/esgvoc_backend/app.py
+++ src/esgvoc_backend/app.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 
 import esgvoc_backend.drs as drs
 import esgvoc_backend.projects as projects
 import esgvoc_backend.universe as universe
 import esgvoc_backend.uris as uris
+
+
+# From FastAPI.
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = (time.perf_counter() - start_time) * 1000.
+    response.headers["X-Process-Time"] = f'{process_time:.1f} ms'
+    return response
 
 
 def create_app() -> FastAPI:
@@ -12,6 +23,7 @@ def create_app() -> FastAPI:
     app.include_router(projects.router)
     app.include_router(drs.router)
     app.include_router(uris.router)
+    app.middleware("http")(add_process_time_header)
     return app
 
 
