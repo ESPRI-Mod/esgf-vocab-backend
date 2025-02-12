9e290767fcfdbdd130c316869b978e06863563fe
Sébastien Gardoll <sebastien@gardoll.fr>
2025-02-12 11:10:07 +0100

    fix: logging configuration for the backend

--- src/esgvoc_backend/app.py
+++ src/esgvoc_backend/app.py
@@ -7,12 +7,12 @@ import esgvoc_backend.drs as drs
 import esgvoc_backend.projects as projects
 import esgvoc_backend.universe as universe
 
-_LOGGER = logging.getLogger("app")
-
 
 def initialization():
+    # Set the root level at INFO (ESGVOC set it at ERROR).
+    logging.getLogger().setLevel(logging.INFO)
+    _LOGGER = logging.getLogger("app")
     _LOGGER.info(f"initialization of process {os.getpid()}")
-
 
 def create_app() -> FastAPI:
     app = FastAPI()
--- src/esgvoc_backend/start.py
+++ src/esgvoc_backend/start.py
@@ -1,20 +1,17 @@
 import logging
 import os
-import sys
 
 import uvicorn
+
+# This import the logging settings of the library ESGVOC.
 
 _DEFAULT_NB_WORKERS = 1
 _UVICORN_WORKERS_VAR_ENV_NAME = "WEB_CONCURRENCY"
 _UVICORN_PORT = 9999
 _APP = "esgvoc_backend.app:app"
+# Set the root level at INFO (ESGVOC set it at ERROR).
+logging.getLogger().setLevel(logging.INFO)
 _LOGGER = logging.getLogger("start")
-
-_LOG_HANDLERS = [logging.StreamHandler(sys.stdout)]
-_LOG_FORMAT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-_LOG_LEVEL = logging.INFO
-logging.basicConfig(level=_LOG_LEVEL, format=_LOG_FORMAT, handlers=_LOG_HANDLERS)
-
 
 def main():
     n_workers = (
