18c0f16019a320764f3adf502ae7198740e07fed
Sébastien Gardoll <sebastien@gardoll.fr>
2025-03-20 19:46:24 +0100

    build: fix docker port fowarding

--- src/esgvoc_backend/start.py
+++ src/esgvoc_backend/start.py
@@ -17,7 +17,7 @@ def main():
 
     uvicorn.run(
         app=_APP,
-        host="127.0.0.1",
+        host="0.0.0.0",  # noqa: S104
         port=_UVICORN_PORT,
         proxy_headers=True,
         forwarded_allow_ips="*",
