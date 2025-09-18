285f5c14e99043ea3318a30748c3a746dc13441c
Sébastien Gardoll <sebastien@gardoll.fr>
2025-09-18 11:27:40 +0200

    test: all project

--- tests/test_js_generator.py
+++ tests/test_js_generator.py
@@ -1,6 +1,7 @@
 import pytest
 
 from esgvoc_backend import jsg
+from tests.api_inputs import project_id  # noqa: F401
 from tests.utils import _test_get, client_factory
 
 router = jsg.router
@@ -11,8 +12,7 @@ def client(request):
     return client_factory(request, router)
 
 
-def test_cmip6_js_generation(client) -> None:
-    project_id = 'cmip6'
+def test_js_generation(client, project_id) -> None:
     url = f'/{project_id}'
     json_result = _test_get(client=client, url=url, params=None, select=False, id=None)
     assert json_result
